bdc8cb699e10a9ac973f60bf142c11d545cc38cf
Paul <pwittschen@gmail.com>
2015-06-25 10:58:43 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 var SIMON = SIMON || {};
 SIMON.game_over = true;
 var colors_reg     = [' ', '#FFAB07', '#E9D558', '#72AD75', '#0E8D94'],
-    colors_bright  = [' ', '#ffd508', '#ffff6e', '#8ed892', '#11b0b9'];
+	colors_bright  = [' ', '#ffd508', '#ffff6e', '#8ed892', '#11b0b9'];
+
+SIMON.simon_array = [];	// array of randomly generated buttons to memorize
+SIMON.score = 0;     	// number score
 
 SIMON.namespace = function(namespace_string) {
 	var parts = namespace_string.split('.'),
@@ -19,20 +22,12 @@ SIMON.namespace = function(namespace_string) {
 	return parent;
 };
 
-// array of randomly generated buttons to memorize
-SIMON.simon_array = [];
-
-// number score
-SIMON.score = 0;
-
 // little start button animation/initiate game - game_loop()
 SIMON.game_started = false;
 $('#start').on('click', function() {
 	if (SIMON.game_started) {
-		console.log('game already started');
 		return;
 	} else {
-
 		$(this).animate({
 			left:'10px'
 		}, 500);
@@ -51,20 +46,16 @@ $('#start').on('click', function() {
 });
 
 SIMON.simon_turn = function(flag) {
-
+	$('.dot').css('background-color', 'white');
 	if (!flag) {
-
 		$("#turn > strong").text("simon");
 		$('.simon_button').removeClass('user_turn');
 
-		// setTimeout used to cause a 2 second delay before 
-		// simon's turn begins
+		// setTimeout used to cause a 1.3 second delay before simon's turn begins
 		setTimeout(function() {
 
-			// generate new button for user to click
+			// generate new button val for user to click
 			var value = Math.floor(Math.random() * 4) + 1, i = 0;
-
-			// add to simon_array
 			SIMON.simon_array.push(value);
 
 			// 'play back' simon's buttons out of array
@@ -81,39 +72,35 @@ SIMON.simon_turn = function(flag) {
 			SIMON.user_turn();
 			SIMON.simon_turn(true);
 		}, 1300);
+	} else {
+		$('#dots_container').append("<div class='dot'></div>");
 	}
 };
-
 // --------------------------------------------
 // what happens during the user's turn
 SIMON.user_turn = function() {
 
 	$('.simon_button').addClass('user_turn');
 	var clicks = 0, length = SIMON.simon_array.length;
+	$('.dot').css('background', 'white');
 
 	var interval_id = setInterval(a, 1000);
 	function a() {
-
 		if (clicks >= length) {
-			console.log('turn over');
 			if (!SIMON.game_over) {
 				SIMON.score++;
 				update_score();
 			}
 			clearInterval(interval_id);
 			SIMON.simon_turn();
-		
 		} else {
 			$("#turn > strong").text("you");
-			console.log('waiting for user to finish turn');
 		}
 	}
 
 	$('.simon_button').unbind('mousedown');
 	$('.simon_button').on('mousedown', function() {
-
-		var button_clicked = $(this).text();
-		button_clicked = parseInt(button_clicked, 10);
+		button_clicked = parseInt( $(this).text() , 10);
 
 		// change button color / play it's corresponding audio
 		$(this).css('background-color', colors_bright[button_clicked]);
@@ -127,6 +114,9 @@ SIMON.user_turn = function() {
 		}
 		// correct user input
 		else {
+			var dots = document.getElementsByClassName('dot');
+			current_dot = dots[clicks];
+			current_dot.style.background = '#434D53';
 			clicks += 1;
 		}
 	});
@@ -135,9 +125,8 @@ SIMON.user_turn = function() {
 		$(this).css('background-color', colors_reg[parseInt($(this).text(), 10)]);
 	});
 };
-
 // --------------------------------------------
-// helper function:
+// helper functions:
 
 /* Button handling */
 function button_on(number) {
@@ -150,10 +139,9 @@ function play_audio(button_num) {
 	sound.play();
 }
 function change_color(button_num) {
-	var button = $("#button_" + button_num.toString()),
-		color = hexc(button.css("background-color"));
-	button.css('background-color', shade_color(color, 25));
-	setTimeout(function() { button.css('background-color', color); }, 700);
+	var button = $("#button_" + button_num.toString());	
+	button.css('background-color', colors_bright[button_num] );
+	setTimeout(function() { button.css('background-color', colors_reg[button_num]); }, 700);
 }
 /* keep score div updated */
 function update_score() {
